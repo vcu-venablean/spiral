33cff5e8788d689dc6467ad9aea8413d841a6cb0
vcu-venablean <venablean@vcu.edu>
2021-04-08 06:14:41 -0400

    Completed homework file with comments

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,20 @@
 def spiralize(number):
-    return_value = 1
+    """Initializing the variable I need for the spiral loop"""
+    n = 1
+    step = 2
+    total = 0
+    since_last = 0
+    """A While loop that builds the spiral and sums up the diagonals"""
+    while n <= number ** 2:
+        total += n
+        n += step
+        since_last += 1
+        if since_last == 4:
+            step += 2
+            since_last = 0
+    """Total is the variable I used that adds up the diagonals of the spiral and gets returned"""
+    return total
 
-    return return_value
+
+"""Printing the sum of the diagonals of a 501 by 501 spiral"""
+print(spiralize(501))
